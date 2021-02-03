6dd298bda46ed81c2d8fa1f806c8261fbd8ec9d8
Daniel_Yepez <dannyypz@gmail.com>
2021-02-03 13:27:17 -0500

    basic backend setup

--- server/app.js
+++ server/app.js
@@ -1,11 +1,33 @@
+require("./db/config");
 const express = require("express"),
-path = require('path'),
-morgan = require('morgan');
-
-const app = express();
+  app = express(),
+  morgan = require("morgan"),
+  cookieParser = require("cookie-parser"),
+  openRoutes = require("./routes/open/index"),
+  path = require("path");
 
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
+// Unauthenticated routes
+app.use("/api/users", openRoutes);
+
+app.use(cookieParser());
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
+}
+
+// We'll add more stuff in between in a little bit.
+
+if (process.env.NODE_ENV === "production") {
+  // Handle React routing, return all requests to React app
+  app.get("*", (request, response) => {
+    response.sendFile(
+      path.resolve(__dirname, "..", "client", "build", "index.html")
+    );
+  });
+}
 
 module.exports = app;
