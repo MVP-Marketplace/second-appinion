54d27037534729ee9ceb33bf02bdc64cc9fc65a5
JadivaM <jmont261@fiu.edu>
2021-02-09 12:02:18 -0500

    added passport middleware in app.js and edited error with quotes incorrectly placed in secure routes endpoint

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const express = require("express"),
   app = express(),
   morgan = require("morgan"),
   cookieParser = require("cookie-parser"),
+  userRouter = require("./routes/secure/users"),
+  passport = require("./middleware/authentication"),
   openRoutes = require("./routes/open/index"),
   path = require("path");
 
@@ -18,6 +20,11 @@ if (process.env.NODE_ENV === "production") {
   // Serve any static files
   app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
 }
+
+//  Authenticated  Routes
+app.use("/api/*", passport.authenticate("jwt", { session: false }));
+
+app.use("/api/users", userRouter);
 
 // We'll add more stuff in between in a little bit.
 
--- server/routes/secure/users.js
+++ server/routes/secure/users.js
@@ -10,7 +10,7 @@ const router = require("express").Router(),
 router.get("/me", getCurrentUser);
 router.patch("/me", updateCurrentUser);
 router.post("/logout", logoutUser);
-router.post("/logoutall, logoutAllDevices");
+router.post("/logoutall", logoutAllDevices);
 router.delete("/", deleteUser);
 
 module.exports = router;
