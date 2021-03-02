55b64c39e97feb12a8ada616c662ea2ae7edc671
Jadivam <Jmont261@fiu.edu>
2021-03-01 21:38:07 -0500

    edit open routes for form

--- server/routes/open/forms.js
+++ server/routes/open/forms.js
@@ -1,15 +1,9 @@
 const router = require('express').Router(),
   {
-    createForm,
-    getSpecificForm,
-    getAllForms,
+    createForm
   } = require('../../controllers/tasks');
 
 router.post('/', createForm);
-
-router.get('/:id', getSpecificForm);
-
-router.get('/', getAllForms);
 
 
 module.exports = router;
