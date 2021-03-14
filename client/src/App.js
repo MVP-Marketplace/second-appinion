8a5cc7ebc0ba442617e6a338c10a650629b74a5b
Jadivam <Jmont261@fiu.edu>
2021-03-14 04:44:44 -0400

    add route for form pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import userForm from "./pages/UserForm";
 import "./App.css";
 
 const App = () => {
@@ -8,12 +9,12 @@ const App = () => {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/form" component={userForm} />
         {/* <Route exact path="/welcome" component={WelcomePage} />
           <Route exact path="/signup" component={SignUpPage} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/resetpassword" component={ResetPassword} />
           <Route exact path="/updatepassword" component={UpdatePassword} />
-          <Route exact path="/form/page/:id" component={Form} />
           <Route exact path="/complete" component={Congrats} />
           <Route exact path="/results" component={Results} /> */}
       </Switch>
