7da4ff95a391d81a0fa40f000576dace0eaee981
Sonia <st.remysonia@gmail.com>
2021-03-17 12:02:28 -0400

    used material ui to create a navbar and menu

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Footer from "../components/Footer";
 import HomeBody from "../components/HomeBody";
+import HamBurger from "../components/HamBurger";
 
 const Home = () => {
   return (
     <>
+      <HamBurger />
       <HomeBody />
       <Footer />
     </>
