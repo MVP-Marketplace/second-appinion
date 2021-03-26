649c8dd55153d95522d22f2c7d3a84568d038183
Jadivam <Jmont261@fiu.edu>
2021-03-26 02:01:45 -0400

    create component folder for form

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Grid, Button } from "@material-ui/core";
 import welcome from "../Images/welcome.png";
 
