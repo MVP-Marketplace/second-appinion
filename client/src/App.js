03ba750edfdf1a8772b14abc75d6db07e060a0a1
Jadivam <Jmont261@fiu.edu>
2021-03-31 22:32:07 -0400

    add complete route and component with prompt

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Welcome from "./components/Welcome";
 import userForm from "./pages/UserForm";
+import ConfirmationPrompt from "./pages/ConfirmationPrompt";
 import "./App.css";
 
 const App = () => {
@@ -11,13 +12,13 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/form" component={userForm} />
+        <Route exact path="/complete" component={ConfirmationPrompt} />
 
         <Route exact path="/welcome" component={Welcome} />
-        <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/login" component={Login} />
+        {/* <Route exact path="/signup" component={SignUp} /> */}
+        {/* <Route exact path="/login" component={Login} /> */}
         {/*<Route exact path="/resetpassword" component={ResetPassword} />
           <Route exact path="/updatepassword" component={UpdatePassword} />
-          <Route exact path="/complete" component={Congrats} />
           <Route exact path="/results" component={Results} /> */}
       </Switch>
     </BrowserRouter>
