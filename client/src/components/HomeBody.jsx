784623dc5aaef17f0e66fb009efb6bbbb400fb72
Daniel_Yepez <dannyypz@gmail.com>
2021-04-22 01:00:44 -0400

    styling updated

--- client/src/components/HomeBody.jsx
+++ client/src/components/HomeBody.jsx
@@ -15,40 +15,45 @@ import { Grid, Button } from "@material-ui/core";
 const HomeBody = () => {
   // const classes = useStyles();
   return (
-    <Grid className="container">
-      <Grid className="headerHome">
-        <h1 className="headerTitle">Get a second opinion</h1>
-        <h3 className="headerSubTitle">Meet your new Specialist</h3>
-        {/* <img className="homeImg" src={Home} alt="Customer in a Dentist Chair"/> */}
+    <>
+      <Grid className="container">
+        <Grid className="headerHome">
+          <h1 className="headerTitle">Get a second opinion</h1>
+          <h3 className="headerSubTitle">Meet your new Specialist</h3>
+          {/* <img className="homeImg" src={Home} alt="Customer in a Dentist Chair"/> */}
+        </Grid>
+        <img className="homeImg" src={Home} alt="Customer in a Dentist Chair" />
+        <Grid>
+          <Link className="button-link" to="/form">
+            <Button variant="contained" size="large" id="bookbutton">
+              START HERE!
+            </Button>
+          </Link>
+        </Grid>
+        <div>
+          <h1 className="reasonTitle">Why us?</h1>
+        </div>
       </Grid>
-      <img className="homeImg" src={Home} alt="Customer in a Dentist Chair" />
-      <Grid>
-        <Link className="button-link" to="/form">
-          <Button variant="contained" size="large" id="bookbutton">
-            START HERE!
-          </Button>
-        </Link>
-      </Grid>
-      <div>
-        <h1 className="reasonTitle">Why us?</h1>
-      </div>
       <Grid className="btmBody">
         <Grid className="picAndText">
           <img className="pic" src={time} alt="Clock" />
           <h4 className="picSubTitle">
-            Timely Second Opinion From Field Experts
+            Timely Second <br /> Opinion From <br /> Field Experts
           </h4>
         </Grid>
         <Grid className="picAndText">
           <img className="pic" src={customer} alt="Customer on the Phone" />
-          <h4 className="picSubTitle">Build Trusting Relationships</h4>
+          <h4 className="picSubTitle">
+            Build Trusting <br />
+            Relationships
+          </h4>
         </Grid>
         <Grid className="picAndText">
           <img className="pic" src={pill} alt="Pill" />
           <h4 className="picSubTitle">Service Promise</h4>
         </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
 
--- server/routes/open/forms.js
+++ server/routes/open/forms.js
@@ -1,7 +1,7 @@
 const router = require("express").Router(),
-  { createForm, uploadXray } = require("../../controllers/forms");
+  { createForm } = require("../../controllers/forms");
 
-router.post("/", createForm, uploadXray);
+router.post("/", createForm);
 // router.post("/upload", uploadXray);
 
 module.exports = router;
